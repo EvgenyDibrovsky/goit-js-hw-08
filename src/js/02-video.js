5737e33d1b91fbe15b89da33353be8c9977087bc
Evgeny Dibrovsky <ed190687@gmail.com>
2022-09-23 22:25:47 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,17 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',  throttle( e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-    }, 1000)
-    );
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-.catch(function (error) {
-    console.error(error)
-});
+function onPlay(e) {
+    localStorage.setItem("videoplayer-current-time", e.seconds);
+    
+    if (e.seconds === e.duration) {
+        localStorage.removeItem("videoplayer-current-time");
+    }
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
